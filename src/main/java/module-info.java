1a7bab05bb8b69e105cbde6fb7efd8ad6deec05d
manwa <manwa.tse@student.fontys.nl>
2019-02-19 10:55:44 +0100

    test build

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,9 @@
 module AnimalShelter {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
 
-
+    opens  org.openjfx to javafx.fxml;
+    exports  org.openjfx;
 
 }
---
+++ src/main/java/org/openjfx/main.java
@@ -0,0 +1,17 @@
+package org.openjfx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
